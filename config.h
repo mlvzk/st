613ca300d0ea3e67fded71110cb0253232e2e210
mlvzk <mlvzk@protonmail.com>
2019-04-21 18:07:59 +0200

    change font

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 //static char *font = "SauceCodePro Nerd Font Mono:style=Light:pixelsize=18:antialias=true:autohint=true";
-static char *font = "SF Mono:pixelsize=14";
+static char *font = "monospace:size=11";
 static int borderpx = 10;
 
 /*
